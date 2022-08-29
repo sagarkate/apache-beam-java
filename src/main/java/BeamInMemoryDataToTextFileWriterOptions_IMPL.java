0b9cc98a7261391c008d193fb49e63b4d9fb3695
Sagar <sagar.kate01@gmail.com>
2022-08-29 21:32:47 +0530

    - Created Custom PipelineOptions interface. - Used custom PipelineOptions interface to handle pipeline options in BeamInMemoryDataToTextFileWriterWithOptions.java

---
+++ src/main/java/BeamInMemoryDataToTextFileWriterOptions_IMPL.java
@@ -0,0 +1,10 @@
+import org.apache.beam.sdk.options.PipelineOptions;
+
+public interface BeamInMemoryDataToTextFileWriterOptions_IMPL extends PipelineOptions {
+
+    String getOutput();
+    void setOutput(String output);
+
+    String getSuffix();
+    void setSuffix(String suffix);
+}
