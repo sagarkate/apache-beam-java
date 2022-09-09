fb4cc6b11cccace8ec2ca8fe5bbe7429af9ce227
Sagar <sagar.kate01@gmail.com>
2022-09-09 16:59:47 +0530

    - Added Default values to input and output file path options - Added Simple Pipeline that create PCollection from a List of Strings and prints it on the console

--- src/main/java/BeamTextFileReaderWriterOptions_IMPL.java
+++ src/main/java/BeamTextFileReaderWriterOptions_IMPL.java
@@ -1,12 +1,16 @@
+import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.ValueProvider;
 
 public interface BeamTextFileReaderWriterOptions_IMPL extends PipelineOptions {
     @Description("Input File Path")
+    @Default.String("gs://mybucket/input")
     ValueProvider<String> getInput();
     void setInput(ValueProvider<String> input);
 
+    @Description("Output File Path")
+    @Default.String("gs://mybucket/output")
     ValueProvider<String> getOutput();
     void setOutput(ValueProvider<String> output);
 }
---
+++ src/main/java/SimplePipeline.java
@@ -0,0 +1,43 @@
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SimplePipeline {
+    public static void main(String[] args) {
+        PipelineOptionsFactory.register(BeamTextFileReaderWriterOptions_IMPL.class);
+        BeamTextFileReaderWriterOptions_IMPL pipelineOptions = PipelineOptionsFactory.fromArgs(args).withValidation().as(BeamTextFileReaderWriterOptions_IMPL.class);
+        Pipeline pipeline = Pipeline.create(pipelineOptions);
+
+        List<String> lines = Arrays.asList(
+                "This is first line",
+                "This is second line",
+                "This is 3rd line");
+
+        /*
+        * - The Coder specifies how the elements in the Collection should be encoded.
+        * - Here, we are specifying that String should encoded using StringUtf8Coder.
+        * - Beam needs to be able to encode each element as a byte string so that
+        *   elements can be passed around to distributed workers.
+        * */
+        PCollection<String> linesPcoll = pipeline.apply("Create PCollection", Create.of(lines)).setCoder(StringUtf8Coder.of());
+        linesPcoll.apply(
+                "Print",
+                ParDo.of(new PrintElementsFn()));
+
+        pipeline.run().waitUntilFinish();
+    }
+
+    public static class PrintElementsFn extends DoFn<String, Void> {
+        @ProcessElement
+        public void processElement(@Element String line) {
+            System.out.println(line);
+        }
+    }
+}
