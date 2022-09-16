dccc51aecb593da50aa9ad9fb7f5bcc96137e09e
Sagar <sagar.kate01@gmail.com>
2022-09-16 18:19:14 +0530

    - Moved Tranformation logic transform/BqRawToStagingTransformFn.java

--- src/main/java/com/practice/gcp/pipelines/MyPipeline.java
+++ src/main/java/com/practice/gcp/pipelines/MyPipeline.java
@@ -5,21 +5,14 @@ import com.practice.gcp.options.MyOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.values.PCollection;
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableSchema;
-import com.google.gson.Gson;
+import com.practice.gcp.transform.BqRawToStagingTransformFn;
 import org.apache.beam.sdk.options.ValueProvider.NestedValueProvider;
-import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,79 +21,7 @@ import org.apache.beam.sdk.transforms.SerializableFunction;
 public class MyPipeline {
 
     private static final Logger LOG = LoggerFactory.getLogger(MyPipeline.class);
-
-    public static class TransformFn extends DoFn<TableRow, TableRow> {
-        ValueProvider<String> schema;
-
-        public TransformFn(ValueProvider<String> schema) {
-            this.schema = schema;
-        }
-
-        @ProcessElement
-        public void processElement(@Element TableRow inputRow, ProcessContext c, OutputReceiver<TableRow> outputReceiver) {
-            LOG.info("MyPipelineTransformLog - Inside TransformFn - ");
-
-            LOG.info(String.format("MyPipelineTransformLog - Inside TransformFn - inputRow: ", new Gson().toJson(inputRow)));
-            LOG.info(String.format("MyPipelineTransformLog - Inside TransformFn - inputRow keys: %s", inputRow.keySet()));
-            LOG.info(String.format("MyPipelineTransformLog - Inside TransformFn - inputRow values: %s", inputRow.values()));
-
-            TableRow transformedRow = new TableRow();
-            if (schema.isAccessible()) {
-                LOG.info("MyPipelineTransformLog - Inside TransformFn.. inputSchema - " + schema);
-                String[] fields = schema.get().split("\\|");
-
-                LOG.info("MyPipelineTransformLog - Inside TransformFn - fields from schema string split('\\|') - " + Arrays.toString(fields));
-
-                for(String field: fields) {
-                    String[] fieldNameAndfieldType = field.split(":");
-                    LOG.info("MyPipelineTransformLog - Inside TransformFn - fieldNameAndfieldType from field string split(':') - " + Arrays.toString(fieldNameAndfieldType));
-                    String fieldName = fieldNameAndfieldType[0];
-                    String fieldType = fieldNameAndfieldType[1];
-                    LOG.info(String.format("MyPipelineTransformLog - Inside TransformFn - fieldName : %s, fieldType: %s ", fieldName, fieldType));
-                    LOG.info(String.format("MyPipelineTransformLog - Inside TransformFn - inputRow field value: %s", (String) inputRow.get(fieldName)));
-                    try {
-                        switch (fieldType) {
-                            case "INTEGER":
-                            case "LONG":
-                                transformedRow.set(fieldName, Long.parseLong((String) inputRow.get(fieldName)));
-                                break;
-                            case "DATE":
-                                transformedRow.set(fieldName, LocalDate.parse((String) inputRow.get(fieldName)).toString());
-                                break;
-                            case "DATETIME":
-                                transformedRow.set(fieldName, LocalDateTime.parse((String) inputRow.get(fieldName)).toString());
-                                break;
-                            case "TIMESTAMP":
-                                transformedRow.set(fieldName, DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse((String) inputRow.get(fieldName), Instant::from).toString());
-                                break;
-                            case "STRING":
-                                transformedRow.set(fieldName, inputRow.get(fieldName));
-                                break;
-                            case "BOOLEAN":
-                                transformedRow.set(fieldName, Boolean.parseBoolean((String) inputRow.get(fieldName)));
-                                break;
-                            case "FLOAT":
-                                transformedRow.set(fieldName, Double.parseDouble((String) inputRow.get(fieldName)));
-                                break;
-                            case "NUMERIC":
-                                transformedRow.set(fieldName, new BigDecimal((String) inputRow.get(fieldName)).toString());
-                                break;
-                            default:
-                                throw new Exception(String.format("Data type %s of field %s is not supported.. Please use one of the below data types: \n [INTEGER|LONG|DATE|DATETIME|TIMESTAMP|STRING|BOOLEAN|FLOAT|NUMERIC]", fieldType, fieldName));
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-                // transformedRow.set("u_desc", Instant.now());
-                transformedRow.set("record_load_ts", DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(Instant.now().toString(), Instant::from).toString());
-                LOG.info(String.format("MyPipelineTransformLog - Inside TransformFn - transformedRow keys: %s", transformedRow.keySet()));
-                LOG.info(String.format("MyPipelineTransformLog - Inside TransformFn - transformedRow values: %s", transformedRow.values()));
-            }
-
-            outputReceiver.output(transformedRow);
-        }
-    }
+    private static final String SCHEMA_FIELD_DELIMITER = "\\|";
 
     public static void main(String[] args) {
         PipelineOptionsFactory.register(MyOptions.class);
@@ -120,7 +41,9 @@ public class MyPipeline {
 
         PCollection<TableRow> transformedData = rawInput.apply(
                 "Transform Raw Input",
-                ParDo.of(new TransformFn(pipelineOptions.getSchema()))
+                ParDo.of(new BqRawToStagingTransformFn(
+                        pipelineOptions.getSchema(),
+                        SCHEMA_FIELD_DELIMITER))
         );
 
         transformedData.apply(
@@ -132,7 +55,7 @@ public class MyPipeline {
                             public TableSchema apply(String schema) {
                                 LOG.info("MyPipelineTransformLog - Input schema: " + schema);
                                 TableSchema tableSchema = new TableSchema();
-                                List<String> inputFields = Arrays.asList(schema.split("\\|"));
+                                List<String> inputFields = Arrays.asList(schema.split(SCHEMA_FIELD_DELIMITER));
                                 LOG.info("MyPipelineTransformLog - input fields: "+inputFields);
                                 List<TableFieldSchema> tableSchemaFields = new ArrayList<>();
                                 for (String inputField : inputFields) {
@@ -150,6 +73,7 @@ public class MyPipeline {
                         }))
                         .withCreateDisposition(pipelineOptions.getCreateDisposition())
                         .withWriteDisposition(pipelineOptions.getWriteDisposition())
+
         );
 
         try {
